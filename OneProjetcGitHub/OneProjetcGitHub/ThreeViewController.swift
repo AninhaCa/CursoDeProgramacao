126e53e98b4a297929203c509d795643e1b6dd88
Aninha <aninhapss2000@gmail.com>
2023-03-07 23:06:27 -0300

    Added button back, button follow

--- OneProjetcGitHub/OneProjetcGitHub/ThreeViewController.swift
+++ OneProjetcGitHub/OneProjetcGitHub/ThreeViewController.swift
@@ -22,18 +22,14 @@ class ThreeViewController: UIViewController {
     }
     
     @IBAction func back(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
     }
     
     @IBAction func follow(_ sender: Any) {
+        if let four = self.storyboard?.instantiateViewController(identifier: "four") as? FourViewController {
+            four.rockFour = self.rockThree
+            followButton.backgroundColor = .red
+            self.navigationController?.pushViewController(four, animated: true)
+        }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
